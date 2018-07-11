d169f08a5d0f44e951e670b952d2a4f7c6eb2daf
jorah <jash@moonjoy.net>
2018-07-11 17:27:30 +0700

    day5, done..

---
+++ day5_js_proto_ao_vo/prototype.js
@@ -0,0 +1,50 @@
+var env = (function () {
+
+	function Person () {
+		this.run = function () {
+			return console.log('I can run')
+		}
+		this.think = function () {
+			return  console.log('I can think')
+		}
+	}
+
+	function Pastor () {
+		this.action = function () {
+			console.log('I can treat')
+		}
+	}
+
+	function Women () { 
+		this.sex = 'women'
+		this.special = function () {
+			console.log('I can make i baby')
+		}
+	}
+
+	var person = new Person()
+	Women.prototype = person
+	var sansa = new Women() 
+
+	function test () { 
+		console.log('can I run ?') 
+		sansa.run()
+		console.log('can I think ?') 
+		sansa.think()
+		console.log('if sansa proto equal person', sansa.__proto__ === person)
+	} 
+
+	function func_proto () {
+		// Attension: if Array call __proto__ mean here Array is a Object, such as all is Object in js.
+		// Function also a object that __proto__ assign Function.prototype.
+		return Array.__proto__ === Function.prototype 
+	}
+
+	function main() {
+		test()
+		console.log('func proto', func_proto())
+	}
+
+	main() 
+
+})()
